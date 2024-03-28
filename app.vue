8e1fed77313724e8b0bd141f4becc4514b7c6520
Gary Woodfine <gary.woodfine@threenine.co.uk>
2024-03-28 17:36:23 +0000

    removed welcome

--- app.vue
+++ app.vue
@@ -2,5 +2,6 @@
   <div>
        <h1>Nostrfi.net</h1>
     <p>Walled gardens became prisons, and nostr is the first step towards tearing down the prison walls.</p>
+    <p>test</p>
   </div>
 </template>
