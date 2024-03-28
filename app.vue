31df07fc499074834d16012d8e302f42eb328266
Gary Woodfine <gary.woodfine@threenine.co.uk>
2024-03-28 17:52:46 +0000

    Update styling and add tailwindcss module
    Updated styling in app.vue for the website title and description. Also, a tailwindcss module has been added to nuxt.config.ts. This commit also includes updates to the yarn.lock file, mainly additions related to tailwindcss.

--- app.vue
+++ app.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-       <h1>Nostrfi.net</h1>
-    <p>Walled gardens became prisons, and nostr is the first step towards tearing down the prison walls.</p>
+       <h1 class="text-2xl font-bold underline">Nostrfi.net</h1>
+    <p class="prose prose-2xl">Walled gardens became prisons, and nostr is the first step towards tearing down the prison walls.</p>
     <p>test</p>
   </div>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms')
+  ],
+} satisfies Config
+
